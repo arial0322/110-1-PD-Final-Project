6bc7b3f8db5cf80456f22e937a11f83f59feacdb
xiang <67159721+xiaaaang1126@users.noreply.github.com>
2021-12-22 16:19:55 +0800

    Update mapGenerator.cpp

--- mapGenerator.cpp
+++ mapGenerator.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
-#include <stdlib.h>			// srand()
-#include <time.h>			// time()
+#include <stdlib.h>      // srand()
+#include <time.h>        // time()
 using namespace std;
 
-void showBoard();
+void showMap();
 
-int board[9][9] = {0};		// initialise map
-int counter = 1;			// the number of map already generated
-int maxMapNumber = 1;			// how many map you want to generate
+int map[9][9] = {0};      // initialise map
+int counter = 1;          // the number of map already generated
+int maxMapNumber = 1;     // how many map you want to generate
 
-// print out the whole board
-void showBoard()
+// print out the whole map
+void showMap()
 {
 	for(int i = 0; i < 9; i++)
 	{
 		for(int j = 0; j < 9; j++)
-			cout << board[i][j] << " ";
+			cout << map[i][j] << " ";
 		cout << endl;
 	}
 	cout << endl;
 }
 
-
+// check if the entry is legal
 bool check()
 {
 	// check the row
@@ -31,11 +31,11 @@ bool check()
 
 		for(int j = 0; j < 9; j++)
 		{
-			if(board[i][j] != 0)
+			if(map[i][j] != 0)
 			{
-				if(tempArr[board[i][j] - 1] == 0)
+				if(tempArr[map[i][j] - 1] == 0)
 				{
-					tempArr[board[i][j] - 1] = 1;
+					tempArr[map[i][j] - 1] = 1;
 				}
 				else
 				{
@@ -52,11 +52,11 @@ bool check()
 
 		for(int i = 0; i < 9; i++)
 		{
-			if(board[i][j] != 0)
+			if(map[i][j] != 0)
 			{
-				if(tempArr[board[i][j] - 1] == 0)
+				if(tempArr[map[i][j] - 1] == 0)
 				{
-					tempArr[board[i][j] - 1] = 1;
+					tempArr[map[i][j] - 1] = 1;
 				}
 				else
 				{
@@ -77,11 +77,11 @@ bool check()
 			{
 				for(int j = 0; j < 3; j++)
 				{
-					if(board[m + i][n + j] != 0)
+					if(map[m + i][n + j] != 0)
 					{
-						if(tempArr[board[m + i][n + j] - 1] == 0)
+						if(tempArr[map[m + i][n + j] - 1] == 0)
 						{
-							tempArr[board[m + i][n + j] - 1] = 1;
+							tempArr[map[m + i][n + j] - 1] = 1;
 						}
 						else
 						{
@@ -104,7 +104,7 @@ bool isMapFull()
 
 	for(int i = 0; i < 9; i++)
 		for(int j = 0; j < 9; j++)
-			if(board[i][j] != 0)
+			if(map[i][j] != 0)
 				flag[i*9 + j] = 1;
 
 	for(int i = 0; i < 81; i++)
@@ -115,7 +115,7 @@ bool isMapFull()
 
   for(int i = 0; i < 9; i++)
     for(int j = 0; j < 9; j++)
-      if(board[i][j] == 0)
+      if(map[i][j] == 0)
         return false;
   return true;
 }
@@ -143,23 +143,24 @@ void generateMap(int curIndex)
 		if(counter <= maxMapNumber)
 		{
 			cout << "SudoMap " << counter++ << endl;
-			showBoard();
+			showMap();
 			return;
 		}
 	}
 
+  // try assign number 1~9 to the empty entry
 	srand((unsigned int)time(0));
 	for(int i = 1; i < 10; i++)
 	{
-		board[(int)(curIndex / 9)][curIndex % 9] = i;
+		map[(int)(curIndex / 9)][curIndex % 9] = i;
 		if(check())	// if curNumber is legal.
 		{
 			// cout << "curLevel:" << curIndex << endl;
-			// showBoard();
+			// showMap();
 			generateMap(curIndex + 1);
 		}
 	}
-	board[(int)(curIndex / 9)][curIndex % 9] = 0;
+	map[(int)(curIndex / 9)][curIndex % 9] = 0;
 
 	return;
 }
@@ -178,7 +179,7 @@ void generateRow1()
 		if(numberFlag[curNumber - 1] == 0)
 		{
 			numberFlag[curNumber - 1] = 1;
-			board[counter / 9][counter % 9] = curNumber;  // ** why not change "counter / 9" to 0?
+			map[counter / 9][counter % 9] = curNumber;  // ** why not change "counter / 9" to 0?
 			counter++;
 		}
 	}
@@ -186,11 +187,11 @@ void generateRow1()
 
 int main()
 {
-	cout << "enter the number of board you want to generate: ";
+	cout << "enter the number of map you want to generate: ";
 	cin >> maxMapNumber;
 	cout << "generating first line of the map..." << endl;
 	generateRow1();
-	cout << "generating the full board..." << endl;
+	cout << "generating the full map..." << endl;
 	generateMap(9);
 
 	return 0;
