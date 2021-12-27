21849536b8a099de62eff11bd29300d446067161
xiang <67159721+xiaaaang1126@users.noreply.github.com>
2021-12-27 15:00:59 +0800

    Update mapGenerator.cpp
    12/27 在第二次開會後的版本
    
    - 只需要印出一張盤面即可（但我沒有把 counter 的部分整個刪除，為了不更改到中止條件，我取巧的調整參數而已）
    - 這份檔案尚未將一些不必要的輸出部分刪除

--- mapGenerator.cpp
+++ mapGenerator.cpp
@@ -28,71 +28,45 @@ bool check()
 	for(int i = 0; i < 9; i++)
 	{
 		int tempArr[9] = {0};
-
 		for(int j = 0; j < 9; j++)
-		{
 			if(map[i][j] != 0)
 			{
 				if(tempArr[map[i][j] - 1] == 0)
-				{
 					tempArr[map[i][j] - 1] = 1;
-				}
 				else
-				{
 					return false;
-				}
 			}
-		}
 	}
 
 	//check the col
 	for(int j = 0; j < 9; j++)
 	{
 		int tempArr[9] = {0};
-
 		for(int i = 0; i < 9; i++)
-		{
 			if(map[i][j] != 0)
 			{
 				if(tempArr[map[i][j] - 1] == 0)
-				{
 					tempArr[map[i][j] - 1] = 1;
-				}
 				else
-				{
 					return false;
-				}
 			}
-		}
 	}
 
 	//check the 9-box
 	for(int m = 0; m < 9; m += 3)
-	{
 		for(int n = 0; n < 9; n += 3)
 		{
 			int tempArr[9] = {0};
-
 			for(int i = 0; i < 3; i++)
-			{
 				for(int j = 0; j < 3; j++)
-				{
 					if(map[m + i][n + j] != 0)
 					{
 						if(tempArr[map[m + i][n + j] - 1] == 0)
-						{
 							tempArr[map[m + i][n + j] - 1] = 1;
-						}
 						else
-						{
 							return false;
-						}
 					}
-				}
-			}
 		}
-	}
-
 	return true;
 }
 
@@ -100,85 +74,86 @@ bool check()
 bool isMapFull()
 {
   for(int i = 0; i < 9; i++)
-      for(int j = 0; j < 9; j++)
-          if(map[i][j] == 0)
-              return false;
+    for(int j = 0; j < 9; j++)
+      if(map[i][j] == 0)
+        return false;
   return true;
 }
 
 // check if all the numbers in one-dimension array are non-zero
 bool checkIsNumberFull(int arr[9])
 {
-    for(int i = 0; i < 9; i++)
-	if(arr[i] == 0)
-	    return false;
-	return true;
+  for(int i = 0; i < 9; i++)
+    if(arr[i] == 0)
+      return false;
+  return true;
 }
 
 // return a random number between [1, 9]
 int getRandNumber()
 {
-    return rand()%9 + 1;
+  return rand()%9 + 1;
 }
 
 void generateMap(int curIndex)
 {
-    // if the map hasn't been filled out, then continue cram number in
-    if(isMapFull())
+  // if the map hasn't been filled out, then continue cram number in
+  if(isMapFull())
+  {
+    if(counter <= maxMapNumber)
     {
-	if(counter <= maxMapNumber)
-	    {
-	        cout << "SudoMap " << counter++ << endl;
-		showMap();
-		return;
-	    }
-	}
+      // cout << "SudoMap " << counter++ << endl;
+      counter++;
+      showMap();
+      return;
+      }
+  }
 
-    // try assign number 1~9 to the empty entry
-    srand((unsigned int)time(0));
-    for(int i = 1; i < 10; i++)
+  // try assign number 1~9 to the empty entry
+  srand((unsigned int)time(0));
+  for(int i = 1; i < 10; i++)
+  {
+    map[(int)(curIndex / 9)][curIndex % 9] = i;
+    if(check())	// if curNumber is legal.
     {
-	map[(int)(curIndex / 9)][curIndex % 9] = i;
-	if(check())	// if curNumber is legal.
-	{
-	    // cout << "curLevel:" << curIndex << endl;
-	    // showMap();
-	    generateMap(curIndex + 1);
-	}
+      // cout << "curLevel:" << curIndex << endl;
+      // showMap();
+      generateMap(curIndex + 1);
     }
-    map[(int)(curIndex / 9)][curIndex % 9] = 0;
+  }
 
-    return;
+  map[(int)(curIndex / 9)][curIndex % 9] = 0;
+  return;
 }
 
 // assign numbers to the first row
 void generateRow1()
 {
-    int numberFlag[9] = {0};
-    int curNumber;
-    int counter = 0;
+  int numberFlag[9] = {0};
+  int curNumber;
+  int counter = 0;
 
-    srand((unsigned)time(NULL));
-    while(!checkIsNumberFull(numberFlag))
+  srand((unsigned)time(NULL));
+  while(!checkIsNumberFull(numberFlag))
+  {
+    curNumber = getRandNumber();
+    if(numberFlag[curNumber - 1] == 0)
     {
-	curNumber = getRandNumber();
-	if(numberFlag[curNumber - 1] == 0)
-	{
-	    numberFlag[curNumber - 1] = 1;
-	    map[counter / 9][counter % 9] = curNumber;  // ** why not change "counter / 9" to 0?
-	    counter++;
-	}
+      numberFlag[curNumber - 1] = 1;
+      map[counter / 9][counter % 9] = curNumber;  // ** why not change "counter / 9" to 0?
+      counter++;
     }
+  }
 }
 
 int main()
 {
-    cout << "enter the number of map you want to generate: ";
-    cin >> maxMapNumber;
-    cout << "generating first line of the map..." << endl;
-    generateRow1();
-    cout << "generating the full map..." << endl;
-    generateMap(9);
+  // cout << "enter the number of map you want to generate: ";
+  // cin >> maxMapNumber;
+  // cout << "generating first line of the map..." << endl;
+  generateRow1();
+  // cout << "generating the full map..." << endl;
+  generateMap(9);
 
-    return 0;
+  return 0;
 }
